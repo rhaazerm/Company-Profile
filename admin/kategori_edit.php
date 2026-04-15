635c3e130fef4bee14af36d44dbb53cd20e7ff16
rhaazerm <rakha.azhar2003@gmail.com>
2026-04-15 23:20:54 +0800

    Implement category edit and add functionality with improved form handling and validation

--- admin/kategori_edit.php
+++ admin/kategori_edit.php
@@ -21,7 +21,38 @@
         </div>
 
         <div class="section">
+            <div class="container">
+                <?php
+                    $kategori = mysqli_query($conn, "SELECT * FROM tb_category WHERE category_id = '".$_GET['id']."' ");
+                    if (mysqli_num_rows($kategori) == 0) {
+                        echo '<script>window.location="admin/kategori_data.php"</script>';
+                    }
+                    $k = mysqli_fetch_object($kategori);
 
+                ?>
+                <form action="" method="POST">
+                    <h3>Edit Data Kategori</h3>
+                    <fieldset>
+                        <label>Nama Kategori</label>
+                        <input type="text" name="nama" value="<?php echo $k->category_name; ?>" class="form-control" required>
+                    </fieldset>
+                    <fieldset>
+                        <button type="submit" name="submit" id="contact-submit" data-submit="...Sending">Edit</button>
+                </form>
+                <?php
+                    if (isset($_POST['submit'])) {
+                        $nama = ucwords($_POST['nama']);
+
+                        $update = mysqli_query($conn, "UPDATE tb_category SET category_name = '" . $nama . "' WHERE category_id = '" . $k->category_id . "' ");
+
+                        if ($update) {
+                            echo '<script>alert("Edit data berhasil")</script>';
+                            echo '<script>window.location="kategori_data.php"</script>';
+                        } else {
+                            echo 'gagal' . mysqli_error($conn);
+                        }
+                    }
+                ?>
         </div>
     </div>
 </body>
--- admin/kategori_tambah.php
+++ admin/kategori_tambah.php
@@ -22,13 +22,33 @@
 
         <div class="section">
             <div class="container">
+                <?php
+                    $query = mysqli_query($conn, "SELECT * FROM tb_admin WHERE admin_id = '".$_SESSION['id_login']."' ");
+                    $d = mysqli_fetch_object($query);
+                ?>
                 <form action="" method="POST">
+                    <h3>Tambah Data Kategori</h3>
                     <fieldset>
                         <input type="text" name="nama" placeholder="Nama Kategori" class="form-control" required>
                     </fieldset>
                     <fieldset>
-                        <button type="submit" name="submit" id="contact-submit" data-submit="..Sending">Tambah</button>
+                        <button type="submit" name="submit" id="contact-submit" data-submit="...Sending">Tambah</button>
+                    </fieldset>
                 </form>
+                <?php
+                    if (isset($_POST['submit'])) {
+                        $nama = $_POST['nama'];
+
+                        $insert = mysqli_query($conn, "INSERT INTO tb_category VALUES ('', '$nama')");
+
+                        if ($insert) {
+                            echo '<script>alert("Tambah data berhasil")</script>';
+                            echo '<script>window.location="kategori_data.php"</script>';
+                        } else {
+                            echo 'gagal' . mysqli_error($conn);
+                        }
+                    }
+                ?>
             </div>
         </div>
     </div>
